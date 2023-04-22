eca17b9cad87200f5006525a4aeef851aa4a95e3
Welcome Sithole <sitholewb@gmail.com>
2023-04-22 19:57:17 +0200

    add IFileEntity

---
+++ Files.EntityFrameworkCore.Extensions/IFileEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Files.EntityFrameworkCore.Extensions
+{
+	public interface IFileEntity
+	{
+		public Guid Id { get; set; }
+		public Guid FileId { get; set; }
+		public string Name { get; set; }
+		public string MimeType { get; set; }
+		public string Hash { get; set; }
+		public DateTimeOffset TimeStamp { get; set; }
+		public Guid? NextId { get; set; }
+		public int Start { get; set; }
+		public int Length { get; set; }
+		public byte[] Data { get; set; }
+	}
+}
