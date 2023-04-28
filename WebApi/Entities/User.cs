a364aa82190856787bd41f1696a876dbf173093f
Welcome Sithole <sitholewb@gmail.com>
2023-04-28 17:27:32 +0200

    Add User model

---
+++ WebApi/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Entities
+{
+	public class User
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTimeOffset LastModifiedDate { get; set; }
+		public DateTimeOffset CreatedDate { get; set; }
+		public Guid? ImageId { get; set; }
+	}
+}
