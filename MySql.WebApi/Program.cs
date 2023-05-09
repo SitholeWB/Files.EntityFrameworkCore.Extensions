6804de22d260c51a30fb1668127bbf940cf8f177
Welcome Sithole <sitholewb@gmail.com>
2023-05-09 20:03:18 +0200

    remove WeatherForecastController from examples

--- MySql.WebApi/Program.cs
+++ MySql.WebApi/Program.cs
@@ -22,11 +22,8 @@ namespace MySql.WebApi
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+			app.UseSwagger();
+			app.UseSwaggerUI();
 
 			app.UseHttpsRedirection();
 
